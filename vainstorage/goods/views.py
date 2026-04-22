8fd7c4bd5f54120f4dc7efef5596059008fa6fe6
Admin <mnemowizard@gmail.com>
2026-04-22 13:11:50 +0500

    Task1

---
+++ vainstorage/goods/views.py
@@ -0,0 +1,8 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def index(request):
+    return HttpResponse("Hello, world. You're at the polls index.")
+
+def contact(request):
+    return HttpResponse("<h1>CONTACTS</h1>")
